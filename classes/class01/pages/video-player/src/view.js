de56ca3fc34a00ada762b150e5fc85e374f94dd7
rafelis1997 <rafaelheros80@gmail.com>
2023-02-28 15:37:25 -0300

    first commit

---
+++ classes/class01/pages/video-player/src/view.js
@@ -0,0 +1,38 @@
+export default class View {
+  #btnInit = document.querySelector('#init')
+  #statusElement = document.querySelector('#status')
+  #videoFrameCanvas = document.createElement('canvas')
+  #canvasContext = this.#videoFrameCanvas.getContext('2d', { willReadFrequently: true })
+  #videoElement = document.querySelector('#video')
+  
+  getVideoFrame(video) {
+    const canvas = this.#videoFrameCanvas
+    const [width, height] = [video.videoWidth, video.videoHeight]
+    canvas.width = width
+    canvas.height = height
+
+    this.#canvasContext.drawImage(video, 0, 0, width, height)
+    return this.#canvasContext.getImageData(0, 0, width, height)
+  }
+  togglePlayPause({right, left}) {
+    if (right && left) return
+    if (right) {
+      this.#videoElement.play()
+      return
+    }
+    if (left) {
+      this.#videoElement.pause()
+    }
+  }
+
+  enableButton() {
+    this.#btnInit.disabled = false
+  }
+
+  configureOnBtnClick(fn) {
+    this.#btnInit.addEventListener('click', fn)
+  }
+  log(text) {
+    this.#statusElement.innerHTML = text
+  }
+}
