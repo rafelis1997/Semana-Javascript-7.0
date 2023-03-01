0e1fb827da2b98f3dfc363952aaacb2606ee3ca8
rafelis1997 <rafaelheros80@gmail.com>
2023-03-01 16:51:17 -0300

    feat: criar gesto de click e desenhar as mãos em cima da DOM

---
+++ classes/class03/pages/video-player/src/controller.js
@@ -0,0 +1,75 @@
+
+
+export default class Controller {
+  #view
+  #worker
+  #blinkLeftCounter = 0;
+  #blinkRightCounter = 0;
+  #camera
+  constructor({ view, worker, camera }) {
+    this.#view = view
+    this.#camera = camera
+    this.#worker = this.#configureWorker(worker)
+
+    this.#view.configureOnBtnClick(this.onBtnStart.bind(this))
+  }
+
+  static async initialize(deps) {
+    const controller = new Controller(deps)
+    controller.log('not yet detected eye blink! click in the button to start')
+    return controller.init()
+
+  }
+  
+  #configureWorker(worker) {
+    let ready = false
+    worker.onmessage = ({data}) => {
+      if ('READY' === data) {
+        console.log('worker is ready!')
+        this.#view.enableButton()
+        ready = true
+        return;
+      }
+
+      const blinkedRight = data.blinkedRight
+      const blinkedLeft = data.blinkedLeft
+      this.#blinkRightCounter += blinkedRight
+      this.#blinkLeftCounter += blinkedLeft
+      this.#view.togglePlayPause({right:blinkedRight, left:blinkedLeft})
+      console.log(blinkedLeft ? 'blinked left'+ blinkedLeft : 'blinked right'+ blinkedRight)
+    }
+
+    return {
+      send(msg) {
+        if (!ready) return
+        worker.postMessage(msg)
+      }
+    }
+  }
+
+  async init() {
+    console.log('init!!')
+  }
+
+  loop() {
+    const video = this.#camera.video
+    const img = this.#view.getVideoFrame(video)
+    this.#worker.send(img)
+    this.log('detecting eye blink...')
+
+    setTimeout(() => this.loop(), 100)
+  }
+
+  log(text) {
+    const timesRight = `        - blink time: ${this.#blinkRightCounter}`
+    const timesLeft = `        - blink time: ${this.#blinkRightCounter}`
+    this.#view.log(`status: ${text}`.concat(this.#blinkRightCounter ? timesRight : ""))
+  }
+
+  onBtnStart() {
+    this.log('initializing detection...')
+    this.#blinkLeftCounter = 0;
+    this.#blinkRightCounter = 0;
+    this.loop()
+  }
+}
---
+++ classes/class03/pages/video-player/src/worker.js
@@ -0,0 +1,24 @@
+import "https://unpkg.com/@tensorflow/tfjs-core@2.4.0/dist/tf-core.js"
+import "https://unpkg.com/@tensorflow/tfjs-converter@2.4.0/dist/tf-converter.js"
+import "https://unpkg.com/@tensorflow/tfjs-backend-webgl@2.4.0/dist/tf-backend-webgl.js"
+import "https://unpkg.com/@tensorflow-models/face-landmarks-detection@0.0.1/dist/face-landmarks-detection.js"
+
+import Service from "./service.js"
+//no processo principal é window
+//no worker é self
+const {tf, faceLandmarksDetection} = self
+tf.setBackend('webgl')
+
+const service = new Service({
+  faceLandmarksDetection,
+})
+console.log('loading tf model...')
+await service.loadModel()
+console.log('tf model loaded successfully!')
+postMessage('READY')
+
+onmessage = async ({ data: video }) => {
+  const {blinkedLeft, blinkedRight} = await service.handBlinked(video)
+  if ((!blinkedLeft && !blinkedRight) || (blinkedLeft && blinkedRight)) return
+  postMessage({ blinkedLeft, blinkedRight })
+}
